10d0ac0f4eaabff01fa2f1a090ad1487ebc69995
SojihAhmed114 <sojib.ahmed7620@gmail.com>
2024-02-04 14:13:00 +0600

    function defination

---
+++ function.js
@@ -0,0 +1,24 @@
+// function structure and call
+// function meanfullFunctionName(parameter/name) {
+//     any statement
+// }
+
+//function output পাইতে হলে অবশ্যই কল করতে হবে। একবার বার function লিখে অন্য কাজ করার পর সেম function প্রয়োজন হলে শুধু মাত্র function কল করলেই হবে।
+
+//function call system
+//meanfullFunctionName();
+
+
+// function এর সুবিধা : function  অনেক ডায়নামিক ভাবে কাজ করে। যেমন : অন্য কোনো ভাবে 4 এর ‍(square) বর্গ যদি আমরা করি, কিন্তু পরে আবার যদি 5 কিংবা 6 এর বর্গ প্রয়োজন হতে  পারে। তখন আবার নতুন করে কোড লিখা লাগবে। এক বার যেন বার বার না করা লাগে সেই জন্য functin ব্যবহার করা হয়।
+
+
+//ex:
+function testFile() {
+    console.log('hi, i am sojib ahmed saif, from gaibandha'); 
+} 
+
+testFile();
+
+//............................................
+
+
