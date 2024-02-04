39f2674274c44e3eee3395c59e2ffa12c3f36eae
SojihAhmed114 <sojib.ahmed7620@gmail.com>
2024-02-04 14:35:49 +0600

    parameter file added and done

--- function.js
+++ function.js
@@ -1,3 +1,7 @@
+// page Number 1
+
+// function এর সুবিধা : function  অনেক ডায়নামিক ভাবে কাজ করে। যেমন : অন্য কোনো ভাবে 4 এর ‍(square) বর্গ যদি আমরা করি, কিন্তু পরে আবার যদি 5 কিংবা 6 এর বর্গ প্রয়োজন হতে  পারে। তখন আবার নতুন করে কোড লিখা লাগবে। এক বার যেন বার বার না করা লাগে সেই জন্য functin ব্যবহার করা হয়।
+
 // function structure and call
 // function meanfullFunctionName(parameter/name) {
 //     any statement
@@ -6,18 +10,18 @@
 //function output পাইতে হলে অবশ্যই কল করতে হবে। একবার বার function লিখে অন্য কাজ করার পর সেম function প্রয়োজন হলে শুধু মাত্র function কল করলেই হবে।
 
 //function call system
-//meanfullFunctionName();
+//meanfullFunctionName(parameter value);
 
 
-// function এর সুবিধা : function  অনেক ডায়নামিক ভাবে কাজ করে। যেমন : অন্য কোনো ভাবে 4 এর ‍(square) বর্গ যদি আমরা করি, কিন্তু পরে আবার যদি 5 কিংবা 6 এর বর্গ প্রয়োজন হতে  পারে। তখন আবার নতুন করে কোড লিখা লাগবে। এক বার যেন বার বার না করা লাগে সেই জন্য functin ব্যবহার করা হয়।
-
-
-//ex:
-function testFile() {
-    console.log('hi, i am sojib ahmed saif, from gaibandha'); 
+//ex: বিভিন্ন সংখার বর্গ এর উদাহরণ দেওয়া হলো:
+function square(numbers) {
+    let borgo = numbers * numbers;
+    console.log(borgo); 
 } 
 
-testFile();
+square(4); //output: 16
+square(20); //output: 400
+square(45); //output: 2025
 
 //............................................
 
---
+++ parameter.js
@@ -0,0 +1,10 @@
+// page number 2
+
+//parameter: () এর ভিতরে যা লিখা হয় তাই প্যারামিটার । parameter শুধু একটা না প্রয়োজন অনুসারে অনেক নেয়া যায়।
+// Addition (+) with many parameter.
+
+function sum(num1, num2, num3) {
+    const total = num1 + num2 + num3;
+    console.log(total); 
+}
+sum(14, 21,25); //output:60
